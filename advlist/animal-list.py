d6ccf0c900bcdf05935cb968ed3f4a8439b4264f
John Crist <riverfalls@users.noreply.github.com>
2022-02-17 22:41:39 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#Optional challenge
+
+# create list of animals
+list = ["fox","fly","ant","bee","cod","cat","dog","yak","cow","hen","koi","hog","jay","kit"]
+
+print(list)
+print(list[0])
+print(list[13])
+
