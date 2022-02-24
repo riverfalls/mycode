d79e5da9932b99a2fa8defc065fdae9f7b70be3b
John Crist <riverfalls@users.noreply.github.com>
2022-02-24 06:28:11 +0000

    Mini-project if elif else script

--- miniproject/planets.py
+++ miniproject/planets.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-#start of counter
-x = 0
+def main():
+    #start of counter
+    x = 0
 
-print("Let's learn about the planets in our solar system.\n")
+    print("Let's learn about the planets in our solar system.\n")
 
-while True:
-    x = x + 1   #increase counter by +1
-    print("Which planet has 14 moons?")
-    answer = input("Your answer: ")
+    while True:
+        x = x + 1   #increase counter by +1
+        print("Which planet has 14 moons?")
+        answer = input("Your answer: ")
 
-    if answer.lower() == "neptune":
-        print("Correct! Great job!")
-        break
-    elif x == 2:
-        print("No, not " + answer + ". Neptune has 14 moons")
-        break
-    else:
-        print("No, not " + answer + ". Let's try again!\n")
+        if answer.lower() == "neptune":
+            print("Correct! Great job!")
+            break
+        elif x == 2:
+            print("No, not " + answer + ". Neptune has 14 moons")
+            break
+        else:
+            print("No, not " + answer + ". Let's try again!\n")
 
+main()
