6980d896604fea12532ca0517e467ac2470160cf
John Crist <riverfalls@users.noreply.github.com>
2022-02-25 19:13:50 +0000

    Python and physical interfaces

---
+++ challenges/fruitloop.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def main():
+    fruitcompanies= [{"name":"Zesty","employees":["Bryan", "Colin", "Erik", "Greg", "John"]},
+                 {"name":"Ripe.ly","employees":["Kishor", "Leia", "Maria", "Chad"]},
+                 {"name":"FruitBee","employees":["Monte", "Jarrad", "Pemba", "Don"]},
+                 {"name":"JuiceGrove","employees":["Tim", "Travis", "Trung"]}]
+    for x in fruitcompanies:
+        print(f'{x["name"]}   {x["employees"]}')
+    user_in = input("Select company: ").lower()
+    for user_in in fruitcompanies:
+        if 
+        print(f'{user_in["employees"]}')
+
+
+
+main()
